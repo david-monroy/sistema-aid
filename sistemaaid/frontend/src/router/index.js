1b5cb2a52ef65dd72358fd68f7c4e41538817923
David Monroy <djmonroy.17@est.ucab.edu.ve>
2021-08-21 09:11:03 -0400

    Estructura Vue con Participante

--- sistemaaid/frontend/src/router/index.js
+++ sistemaaid/frontend/src/router/index.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+import ListParticipante from '@/components/Participante/ListParticipante'
+import EditParticipante from '@/components/Participante/EditParticipante'
+import DeleteParticipante from '@/components/Participante/DeleteParticipante'
+import NewParticipante from '@/components/Participante/NewParticipante'
+
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +15,27 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/participantes',
+      name: 'ListParticipante',
+      component: ListParticipante
+    },
+    {
+      path: '/participantes',
+      name: 'EditParticipante',
+      component: EditParticipante
+    },
+    {
+      path: `/participantes/:id/delete`,
+      name: 'DeleteParticipante',
+      component: DeleteParticipante
+    },
+    {
+      path: `/participantes/new`,
+      name: 'NewParticipante',
+      component: NewParticipante
     }
-  ]
+  ],
+  mode: 'history'
 })
--- sistemaaid/sistemaaid/settings.py
+++ sistemaaid/sistemaaid/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8080'
+]
 
 # Application definition
 
@@ -38,8 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backend',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -49,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'sistemaaid.urls'

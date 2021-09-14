ec15ed16a7aa66eb23f8ab69da311779e4255b51
David Monroy <djmonroy.17@est.ucab.edu.ve>
2021-09-14 11:43:42 -0400

    Relaciones en modelo Participante

--- sistemaaid/backend/models.py
+++ sistemaaid/backend/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+
+class Carrera(models.Model):
+    nombre = models.CharField(max_length=30)
+    codigo = models.CharField(max_length=20)
+    estaEnUCAB = models.BooleanField()
+
+class Sede(models.Model):
+    nombre = models.CharField(max_length=30)
 
 class Participante(models.Model):
     nombre = models.CharField(max_length=50)
@@ -9,4 +18,10 @@ class Participante(models.Model):
     edad = models.IntegerField(null=True, blank=True)
     telfPrincipal = models.CharField(max_length=15)
     telfSecundario = models.CharField(max_length=15, null=True, blank=True)
+    carreras = models.ManyToManyField(Carrera, through='ParticipanteCarrera')
 
+class ParticipanteCarrera(models.Model):
+    participante = models.ForeignKey(Participante, on_delete=CASCADE)
+    carrera = models.ForeignKey(Carrera, on_delete=CASCADE)
+    semestre = models.IntegerField(null=True, blank=True)
+    esEgresado = models.BooleanField(null=True, blank=True)

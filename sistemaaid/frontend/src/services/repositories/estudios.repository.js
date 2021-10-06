62ae0d9a278303f32458f2113f03cd9ee2604bac
carolinapatino <cdpatino.17@est.ucab.edu.ve>
2021-10-06 16:54:39 -0400

    Añadir ficha tecnica a un estudio

--- sistemaaid/frontend/src/services/repositories/estudios.repository.js
+++ sistemaaid/frontend/src/services/repositories/estudios.repository.js
@@ -3,8 +3,6 @@ import jwt from "../../common/jwt.service";
 
 export default {
   async agregar(data) {
-    console.log(jwt.getAuthHeaderToken())
-    console.log (data)
     let response = await conn.post(`/estudios/`, data, jwt.getAuthHeaderToken());
     return response;
   }
--- sistemaaid/frontend/src/views/estudios/AgregarEstudio.vue
+++ sistemaaid/frontend/src/views/estudios/AgregarEstudio.vue
@@ -2,41 +2,58 @@
   <v-container fluid elevation="0" class="nav-separator">
     <v-row no-gutters justify="center">
       <v-col cols="12" md="8" align="center">
-        <v-stepper>
+        <v-stepper v-model="pasoActual">
           <v-stepper-header class="third accent-1">
-            <v-stepper-step step="1">
+            <v-stepper-step step="1" 
+            :complete="pasoActual>1">
             Ficha Técnica
             </v-stepper-step>
 
             <v-divider></v-divider>
 
-            <v-stepper-step step="2">
+            <v-stepper-step step="2"
+            :complete="pasoActual>2">
             Muestra
             </v-stepper-step>
 
             <v-divider></v-divider>
 
-            <v-stepper-step step="3">
+            <v-stepper-step step="3"
+            :complete="pasoActual>3">
             Metodología
             </v-stepper-step>
 
             <v-divider></v-divider>
 
-            <v-stepper-step step="4">
+            <v-stepper-step step="4"
+            :complete="pasoActual>4">
             Instrumento
             </v-stepper-step>
 
             <v-divider></v-divider>
 
-            <v-stepper-step step="5">
+            <v-stepper-step step="5"
+            :complete="pasoActual>5">
             Flujo de ejecución
             </v-stepper-step>
             
           </v-stepper-header>
 
          <v-stepper-items>
-            <v-stepper-content step="1">
+            <v-stepper-content step="1" >
               <FichaTecnica></FichaTecnica>
+            </v-stepper-content>
+            <v-stepper-content step="2" >
+              <h3>Proximamente</h3>
+            </v-stepper-content>
+            <v-stepper-content step="3" >
+              <h3>Proximamente</h3>
+            </v-stepper-content>
+            <v-stepper-content step="4" >
+              <h3>Proximamente</h3>
+            </v-stepper-content>
+            <v-stepper-content step="5" >
+              <h3>Proximamente</h3>
             </v-stepper-content>
           </v-stepper-items> 
         </v-stepper>
@@ -49,18 +66,24 @@
 
 import FichaTecnica from "../../components/estudios/FichaTecnica.vue";
 import { EventBus } from "../../main.js";
-import axios from 'axios'
 import swal from 'sweetalert'
 import Repository from "../../services/repositories/repositoryFactory";
 const EstudiosRepository = Repository.get("Estudios");
 
 export default {
+  data: () => ({
+    pasoActual: 1
+  }),
   components: {
     FichaTecnica
   },
 
   created() {
-    EventBus.$on("ficha-tecnica", (data) => {
+    EventBus.$on("paso2", (data) => {
+        this.pasoActual += 1;  
+        console.log(data)
+    }),
+    EventBus.$on("registrar", (data) => {
         this.insertarEstudio(data);  
     })
   },
@@ -69,11 +92,11 @@ export default {
     async insertarEstudio(data){
       try{
         await EstudiosRepository.agregar(data);
-        swal("Participante creado satisfactoriamente", "", "success")
+        swal("El estudio ha sido agregado satisfactoriamente", "", "success")
       }
       catch(err){
         console.log(err)
-          swal("Participante no pudo ser creado", "", "error")
+          swal("El estudio no pudo ser agregado", "", "error")
       }
     }
   }

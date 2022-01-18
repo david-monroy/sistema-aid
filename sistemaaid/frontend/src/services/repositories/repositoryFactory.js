a9774bc71a73072ec312d43afc799a49c19d6526
carolinapatino <cdpatino.17@est.ucab.edu.ve>
2022-01-17 21:00:59 -0400

    Merge

--- sistemaaid/frontend/src/services/repositories/repositoryFactory.js
+++ sistemaaid/frontend/src/services/repositories/repositoryFactory.js
@@ -1,16 +1,7 @@
 import AutorizacionRepository from "./autenticacion.repository";
-<<<<<<< HEAD
 import EstudiosRepository from "./Estudios/estudios.repository";
 import EdicionesRepository from "./Estudios/ediciones.repository";
 import MuestraPonderadaRepository from "./Estudios/muestraPonderada.repository";
-
-const repositories = {
-    Autorizacion: AutorizacionRepository,
-    Estudios: EstudiosRepository,
-    Ediciones: EdicionesRepository,
-    MuestraPonderada: MuestraPonderadaRepository
-=======
-import estudiosRepository from "./estudios.repository";
 import ParticipantesRepository from "./participantes.repository";
 import SedesRepository from "./sedes.repository";
 import ColegiosRepository from "./colegios.repository";
@@ -20,14 +11,16 @@ import ParticipanteCarrerasRepository from "./participante_carreras.repository";
 
 const repositories = {
     Autorizacion: AutorizacionRepository,
-    Estudios: estudiosRepository,
+    Estudios: EstudiosRepository,
+    Ediciones: EdicionesRepository,
+    MuestraPonderada: MuestraPonderadaRepository,
+    Autorizacion: AutorizacionRepository,
     Participantes: ParticipantesRepository,
     Sedes: SedesRepository,
     Carreras: CarrerasRepository,
     ParticipanteCarreras: ParticipanteCarrerasRepository,
     Colegios: ColegiosRepository,
-    Lugares: LugaresRepository,
->>>>>>> 2df9ec4ea76c5b2363e64afcc735cbee3d4ae79c
+    Lugares: LugaresRepository
 };
 
 export default {

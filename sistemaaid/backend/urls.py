122adce9d963c3492b37b5887fa4ca5dafe0a347
carolinapatino <cdpatino.17@est.ucab.edu.ve>
2022-05-31 21:42:16 -0400

    Merge branch 'AID-EN3-fix' of https://github.com/david-monroy/sistema-aid into copia-develop
    # Conflicts:
    #	sistemaaid/backend/urls.py
    #	sistemaaid/backend/views/estudios.py

--- sistemaaid/backend/urls.py
+++ sistemaaid/backend/urls.py
@@ -1,15 +1,7 @@
 from rest_framework import routers, urlpatterns
 from . import views
-<<<<<<< HEAD
 from django.urls import path, include
-=======
-from django.urls import path
-<<<<<<< HEAD
->>>>>>> 90e4720eda23bb18385e64b04cefeb1a0ccab7a4
 from .viewsets import *
-=======
-from .viewsets import EstudioViewSet, ParticipanteViewSet, CarreraViewSet, SedeViewSet, ParticipanteCarreraViewSet, ColegioViewSet, EdicionViewSet
->>>>>>> 708883dbad694a00eba065c470171258dd18f353
 from rest_framework_simplejwt import views as jwt_views
 
 router = routers.SimpleRouter()
@@ -20,23 +12,13 @@ router.register('colegios', ColegioViewSet)
 router.register('participantecarreras', ParticipanteCarreraViewSet)
 router.register('estudios', EstudioViewSet)
 router.register('ediciones', EdicionViewSet)
-<<<<<<< HEAD
 router.register('muestraPonderada',MuestraPonderadaViewSet)
-<<<<<<< HEAD
 router.register('metodologia',MetodologiaViewSet)
 router.register('preguntas',PreguntaViewSet)
 router.register('listaCodigo',ListaCodigoViewSet)
 router.register('categoria',CategoriaViewSet)
 router.register('lugares', LugarViewSet)
-<<<<<<< HEAD
-=======
-
->>>>>>> 90e4720eda23bb18385e64b04cefeb1a0ccab7a4
-=======
->>>>>>> 708883dbad694a00eba065c470171258dd18f353
-=======
 router.register('preguntaEdicion', PreguntaEdicionViewSet)
->>>>>>> a33aee54cd3c259c1f9d06de5baaf78c541f536a
 
 urlpatterns = [
     path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -45,7 +27,6 @@ urlpatterns = [
     path('muestraPonderada/cargar/<int:tamanoMuestral>', views.cargarMuestra),
     path('muestraPonderada/insertar/<int:idEdicion>', views.insertarMuestra),
     path('estudios/validarCodigo', views.validarCodigo),
-<<<<<<< HEAD
     path('ediciones/validarCodigo', views.validarCodigo),
     path('categoria/cargar/<int:idLista>', views.cargarListaCodigo),
     path('categoria/consultar/<int:idLista>', views.obtenerCategorias),
@@ -61,13 +42,7 @@ urlpatterns = [
     path('usuarios/', views.obtener_usuarios),
     path('usuarios/grupos/', views.obtener_grupos),
     path('estudios/ediciones/<int:id>', views.obtenerEdiciones),
-<<<<<<< HEAD
-=======
-    path('ediciones/validarCodigo', views.validarCodigo)
->>>>>>> 90e4720eda23bb18385e64b04cefeb1a0ccab7a4
-=======
     path('estudios/seleccionarVariablesRFE/', views.seleccionarVariablesRFE),
->>>>>>> 4c47022cd3a0b138943943605d473169b43d99e6
 ]
 
 urlpatterns += router.urls

64eac20b9ec448ca94d10a860792f3aac3c2e4f6
David Monroy <djmonroy.17@est.ucab.edu.ve>
2022-06-02 11:51:46 -0400

    Dos tipos de graficos

--- sistemaaid/frontend/src/views/reportes/Grafico.vue
+++ sistemaaid/frontend/src/views/reportes/Grafico.vue
@@ -3,11 +3,22 @@
     align="center"
     class="list px-3 mx-auto nav-separator container mt-12"
     style="display: flex; align-items: flex-start"
-  > 
+  >
     <v-col cols="6" sm="6" class="mt-2">
-        <canvas id="chart"></canvas>
+      <div
+        class="form-group"
+        style="display: flex; justify-content: center; width: 100%"
+      >
+        <v-select
+          v-model="tipoGrafico"
+          :items="tiposGrafico"
+          item-text="nombre"
+          item-value="id"
+          label="Tipo de gráfico"
+        ></v-select>
+      </div>
+      <canvas id="chart"></canvas>
     </v-col>
-    
   </v-row>
 </template>
 
@@ -18,37 +29,49 @@ export default {
   name: "Grafico",
   data() {
     return {
-      datos: null,
-    };
-  },
-  methods: {
-    setData() {
-      this.datos = {
-        type: "pie",
+      datos: {
         data: {
-          labels: [
-            "Inscritos",
-            "No inscritos"
-          ],
+          labels: ["Inscritos", "No inscritos"],
           datasets: [
             {
-              label: "Inscripciones 2022",
-              data: [150,215],
-              backgroundColor: [
-                  "rgba(54,73,93,.5)",
-                  "rgba(4,7,93,.5)"
-                  ],
+              label: "Historico Inscripciones",
+              data: [100, 120],
+              backgroundColor: ["rgba(54,73,93,.5)", "rgba(4,7,93,.5)"],
               borderWidth: 3,
             },
           ],
         },
-      };
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        }
+      },
+      tipoGrafico: null,
+      tiposGrafico: [
+        { id: 1, nombre: "Torta" },
+        { id: 2, nombre: "Barras" },
+      ],
+    };
+  },
+  methods: {
+    crearGraficoTorta() {
+      this.datos.type = "pie";
+      const ctx = document.getElementById("chart");
+      new Chart(ctx, this.datos);
+    },
+    crearGraficoBarra() {
+      this.datos.type = "bar";
+      const ctx = document.getElementById("chart");
+      new Chart(ctx, this.datos);
     },
   },
   mounted() {
-    this.setData();
-    const ctx = document.getElementById("chart");
-    new Chart(ctx, this.datos);
+    this.crearGraficoBarra();
+    // const ctx = document.getElementById("chart");
+    // new Chart(ctx, this.datos);
   },
 };
 </script>

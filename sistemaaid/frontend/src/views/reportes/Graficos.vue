a961620c765985228d4a53a0a259bf61c0a730f1
David Monroy <djmonroy.17@est.ucab.edu.ve>
2022-06-11 08:12:45 -0400

    Graficos

--- sistemaaid/frontend/src/views/reportes/Graficos.vue
+++ sistemaaid/frontend/src/views/reportes/Graficos.vue
@@ -6,7 +6,7 @@
       style="display: flex; justify-content: center"
     >
       <v-col cols="12" sm="6" class="mt-4">
-        Seleccione una o dos ediciones para comparar sus resultados
+        Seleccione una o dos ediciones del estudio <b>{{ estudio.nombre }}</b> para comparar sus resultados
         <v-select
           v-model="edicionesSelect"
           :items="ediciones"
@@ -14,6 +14,7 @@
           item-value="id"
           chips
           label="Ediciones"
+          v-on:input="limiteOpciones"
           multiple
           outlined
           class="mt-2"
@@ -22,7 +23,7 @@
     </v-row>
     <v-row
       align="center"
-      class="list px-3 mx-auto nav-separator container mt-12"
+      class="list px-3 mx-auto nav-separator container mt-4"
       style="display: flex; align-items: flex-start"
     >
       <v-col cols="3" class="mt-2">
@@ -54,7 +55,8 @@ export default {
     return {
       estudio_id: null,
       ediciones: [],
-      edicionesSelect: []
+      edicionesSelect: [],
+      estudio: null
     };
   },
   methods: {
@@ -62,9 +64,24 @@ export default {
       this.ediciones = await EstudiosRepository.obtenerEdiciones(id);
       console.log(this.ediciones)
     },
+    limiteOpciones(e) {
+      if(e.length > 2) {
+        console.log('Solo puedes seleccionar dos', e)
+        e.pop()
+      }
+    },
+    async getEstudio(id){
+      let estudios = await EstudiosRepository.consultar();
+      estudios.forEach((est) => {
+        if (est.id == id) {
+          this.estudio = est;
+        }
+      });
+    }
   },
   mounted() {
     this.estudio_id = this.$route.params.id;
+    this.getEstudio(this.estudio_id);
     this.getEdiciones(this.estudio_id);
   },
 };

892f923ee9a51eca89f094238f3a437660afdc39
David Monroy <djmonroy.17@est.ucab.edu.ve>
2022-09-14 20:59:53 -0400

    todos los merge

--- sistemaaid/backend/ia/presentaciones/pruebapresentaciones.py
+++
@@ -1,7 +0,0 @@
-from pptx import Presentation
-
-prs = Presentation('backend/ia/presentaciones/prueba1.pptx')
-slide_layout = prs.slide_layouts[1]
-slide = prs.slides.add_slide(slide_layout)
-slide.shapes.title.text = 'New Title'
-prs.save('backend/ia/presentaciones/pruebapresentaciones.pptx')
